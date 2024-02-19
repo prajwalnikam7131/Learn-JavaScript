a3c091a2853edf8a9b0030e3c65c3ebb88d0f9c0
Prajwal Nikam <89040064+prajwalnikam7131@users.noreply.github.com>
2024-02-19 04:54:32 +0000

    update

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,26 @@
 console.log("Prajwal");
+
+/* 
+    In this chapter learn let, const and var.
+    let's understand with example.
+*/
+
+const accountId = 123231;
+let accountEmail = "newuser@123google.in";
+var accountPassword = "53421";
+accountCity = "Malegon";
+
+
+/* 
+
+*/
+// accountId = 123123; 
+
+/*
+    There are different type's to print output.
+    1) console.log(); 
+    2) document.write();
+    3) console.table([variable_name1, variable_name2, variable_name3, ...]) 
+*/
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);

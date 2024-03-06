d5a8679b02648c3467b08706e78d597436925963
Prajwal Nikam <89040064+prajwalnikam7131@users.noreply.github.com>
2024-03-06 07:07:03 +0000

    main

---
+++ 02_basics/02_object.js
@@ -0,0 +1,41 @@
+// singleton Object (constructor Method):
+const student = new Object();
+// console.log(typeof student, student); // Object 
+
+// literals Method:
+
+/*
+    javaScript Symbols:
+        symbols are immutable and are uniqe.
+*/
+// creating Symbol:
+// const sym = Symbol();
+// console.log(typeof sym); // symbol
+
+
+// you can pass an optional string as its description.
+const sym = Symbol("key1");
+console.log(typeof sym, sym); // symbol Symbol(key1)
+
+// Access Symbol Description:
+const access = sym.description;
+console.log(access); // key1
+
+// Add Symbol as an Object Key:
+// you can add symbol as a key in an object using square bracket [].
+
+let id = Symbol("id");
+
+let persons = {
+    firstName: "prajwal",
+    lastName: "Nikam",
+    age: 24,
+    location: "Malegoan",
+
+    // adding symbol as a key
+    [id]: 123,
+    [sym]: 431
+};
+
+console.log(typeof persons); // object
+

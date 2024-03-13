c36c844296b30d38ac2c9728246c7a2253d03c8e
Prajwal Nikam <89040064+prajwalnikam7131@users.noreply.github.com>
2024-03-13 05:20:42 +0000

    update

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,5 +1,5 @@
 var c = 100; // Global Scope
-// console.log(c);  // 100
+console.log(c);  // 100
 
 {
     let a = 10
@@ -9,19 +9,53 @@ var c = 100; // Global Scope
 
 // console.log(a); // error
 // console.log(b); // error
-// console.log(c);  // 30
+console.log(c);  // 30
 
 function one() {
     const firstFunction = `Run Outer Function`;
 
-    console.log(secondFunction);
+    // console.log(secondFunction); // error
 
     function two() {
         const secondFunction = `Run Inner Function`;
-        
+
         console.log(firstFunction);
     }
+    // console.log(secondFunction); // error
+
     two();
+
+    // console.log(secondFunction); // error
+
 }
 
-one();
+// one();
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/*   how to work function before & after execution.     */
+
+/*
+    Normal Function, before and after both execution acceptable.
+*/
+console.log(addone(5)); // 6
+
+function addone(num) {
+    return num + 1
+}
+
+console.log(addone(5)); // 6
+
+
+/*
+    When we store a function in variable then always after execution.
+    when we try before execution then throw error.
+*/
+
+// console.log(addtwo(5)); // error
+
+const addtwo = function (num) {
+    return num + 2
+}
+
+console.log(addtwo(5)); // 7
